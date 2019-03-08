d77aa4de49a3ae74798e5bfc94211dc9c4a3b547
Hesham Ahmed <heshamahmed87@yahoo.com>
2019-03-08 02:25:23 +0200

    Task-Tags-Component#Classes

--- plugins/c/components/CocPureForms/Partials/CocTag.vue
+++ plugins/c/components/CocPureForms/Partials/CocTag.vue
@@ -1,26 +1,43 @@
 <template>
-	<div>
-		<span style="background-color : #f0f0f0; font-size : 15px; color : black; display: inline-block;  padding : 4px; border-radius : 25px;"><span style="padding : 3px;">Hesham</span><span class="right" style=" border-radius : 100%; background-color : rgba(0,0,0,.15); width : 20px; text-align : center; color:white;">x</span></span>
-
-		<span style="background-color : #f0f0f0; font-size : 15px; color : black; display: inline-block;  padding : 4px; border-radius : 3px;"><span style="padding : 3px;">Hesham</span><span class="right" style=" border-radius : 3px; background-color : rgba(0,0,0,.15); width : 20px; text-align : center; color:white;">x</span></span>
-
-		<span style="background-color : #006eff; font-size : 15px; color : black; display: inline-block;  padding : 4px; border-radius : 35px;"><span style="padding : 3px; color : white;">Hesham</span><span class="right" style=" border-radius : 100%; background-color : rgba(0,0,0,.15); width : 20px; text-align : center; color:white;">x</span></span>
-
-		<span style="background-color : #006eff; font-size : 15px; color : black; display: inline-block;  padding : 4px; border-radius : 3px;"><span style="padding : 3px; color : white">Hesham</span><span class="right" style=" border-radius : 3px; background-color : rgba(0,0,0,.15); width : 20px; text-align : center; color:white;">x</span></span>
-	</div>
+  <div>
+    <span
+      :class="[`coc-${color}-section-${type}`,`coc-${borderRadius}-border-radius`,`coc-text-${fontSize}`]"
+			class="coc-padding-4px"
+      style="display: inline-block; border : 1px solid black;"
+    >Hesham</span>
+  </div>
 </template>
 
 <script>
 export default {
-
-  name: 'CocTag',
-
-  data () {
-    return {
-
+  name: "CocTag",
+  props: {
+    color: {
+      type: String,
+      default: "primary",
+      required: true
+    },
+    type: {
+      type: String,
+      default: "dark",
+      required: true
+		},
+		borderRadius: {
+      type: String,
+      default: "xs",
+      required: true
+		},
+		fontSize: {
+      type: String,
+      default: "normal",
+      required: true
     }
+  },
+
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style lang="css" scoped>
